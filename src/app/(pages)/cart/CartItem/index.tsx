68b428db65705bae9c35fde8369bf5253d6dc557
SourceSensei <source.sensei1205@gmail.com>
2023-12-23 12:55:00 +0000

    phase 1

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -13,11 +13,26 @@ import classes from './index.module.scss'
 const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
   const [quantity, setQuantity] = useState(qty)
 
-  const decrementQty = () => {}
+  const decrementQty = () => {
+    const updatedQty = quantity > 1 ? quantity - 1 : 1
 
-  const incrementQty = () => {}
+    setQuantity(updatedQty)
+    addItemToCart({ product, quantity: Number(updatedQty) })
+  }
 
-  const enterQty = () => {}
+  const incrementQty = () => {
+    const updatedQty = quantity + 1
+
+    setQuantity(updatedQty)
+    addItemToCart({ product, quantity: Number(updatedQty) })
+  }
+
+  const enterQty = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedQty = Number(e.target.value)
+
+    setQuantity(updatedQty)
+    addItemToCart({ product, quantity: Number(updatedQty) })
+  }
 
   return (
     <li className={classes.item} key={title}>
@@ -27,37 +42,46 @@ const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
           <Media className={classes.media} imgClassName={classes.image} resource={metaImage} fill />
         )}
       </Link>
+
       <div className={classes.itemDetails}>
         <div className={classes.titleWrapper}>
           <h6>{title}</h6>
           <Price product={product} button={false} />
         </div>
+
         <div className={classes.quantity}>
           <div className={classes.quantityBtn} onClick={decrementQty}>
             <Image
               src="/assets/icons/minus.svg"
+              alt="minus"
               width={24}
               height={24}
-              alt="minus"
               className={classes.qtnBt}
             />
           </div>
+
           <input
             type="text"
             className={classes.quantityInput}
             value={quantity}
             onChange={enterQty}
           />
+
           <div className={classes.quantityBtn} onClick={incrementQty}>
             <Image
               src="/assets/icons/plus.svg"
+              alt="plus"
               width={24}
               height={24}
-              alt="plus"
               className={classes.qtnBt}
             />
           </div>
         </div>
+      </div>
+
+      <div className={classes.subtotalWrapper}>
+        <Price product={product} button={false} quantity={quantity} />
+        <RemoveFromCartButton product={product} />
       </div>
     </li>
   )
